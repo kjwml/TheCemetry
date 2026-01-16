d75dcce84ddea7aff4233b0ca60a661b7cb3d62d
NC3LADUS <dweibert25@gmail.com>
2026-01-16 02:06:16 +0100

    Animation start, Reworking dialogue
    Bitte alles was auf Dialogue system old drauf ist auf Dialogue manager 1 rüberschieben

---
+++ TheCemetry/Assets/Scenes/Dialogue_System/Dialogue_Attempt2/NPC_Monologue.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System;
+
+[Serializable]
+public class NPC_Monologue
+{
+    public string name;
+    public string text;
+}
+   
--- TheCemetry/Assets/Scenes/Dialogue_System/Dialogue_Manager.cs
+++ TheCemetry/Assets/Scenes/Dialogue_System/Dialogue_Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 
@@ -14,5 +15,32 @@ public class Dialogue_Manager : MonoBehaviour
     public void StartDialogue(Dialogue dialogue)
     {
         Debug.Log("Starting conversation with " + dialogue.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+        void DisplayNextSentence ()
+        {
+            if (sentences.Count == 0)
+            {
+                EndDialogue();
+                return;
+            }
+            string sentence = sentences.Dequeue();
+            Debug.Log(sentence);
+
+        }
+
+        void EndDialogue ()
+        {
+            Debug.Log("End of conversation.");
+        }
     }
 }
