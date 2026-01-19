3f6308dd5fbf3e70101201d86c64416d317da48e
NC3LADUS <dweibert25@gmail.com>
2026-01-19 20:21:33 +0100

    Dialogsystem und Bubble animation
    Ein fehler tritt auf den ich iwie nicht beheben kann

--- TheCemetry/Assets/Scenes/Dialogue_System/Dialogue_Attempt2/NPC_Monologue.cs
+++ TheCemetry/Assets/Scenes/Dialogue_System/Dialogue_Attempt2/NPC_Monologue.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 using System;
 
 [Serializable]
+/// <summary>
+/// Data for dialog fragment (participant + text)
+/// </summary>
 public class NPC_Monologue
 {
     public string name;
-    public string text;
+
+    [TextArea(3, 10)]
+    public string[] text;
 }
    
