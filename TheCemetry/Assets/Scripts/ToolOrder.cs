488539f66c9abfa732df1693ca26325f33e0238a
KJ <myt9dwy5bv@privaterelay.appleid.com>
2026-02-06 16:40:24 +0100

    Reihenfolge
    habe anstatt es mit visual codes mal mit versucht meinen komplexen visualcode baustein als c# code umzuwandeln und es hat funktioniert das einezige was nun noch problematisch sind, sind kleine lags

---
+++ TheCemetry/Assets/Scripts/ToolOrder.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class ToolOrder : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform BrownDirtParent;
+    public Transform GreenDirtParent;
+    public Transform RockParent;
+    public GameObject spongeTool;
+    public GameObject brushTool;
+    public GameObject shovelTool;
+    public GameObject wateringCanTool;
+    public GameObject clothTool;
+    
+    void Start()
+    {
+            spongeTool.SetActive(true);
+            brushTool.SetActive(false);
+            shovelTool.SetActive(false);
+            wateringCanTool.SetActive(false);
+            clothTool.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (BrownDirtParent.childCount ==0 && spongeTool.gameObject.activeSelf)
+        {
+            spongeTool.SetActive(false);
+            brushTool.SetActive(true);
+            shovelTool.SetActive(false);
+            wateringCanTool.SetActive(false);
+            clothTool.SetActive(false);
+        }
+        else
+        {
+            spongeTool.SetActive(true);
+            brushTool.SetActive(false);
+            shovelTool.SetActive(false);
+            wateringCanTool.SetActive(false);
+            clothTool.SetActive(false);
+        }
+
+        if (GreenDirtParent.childCount ==0 && brushTool.gameObject.activeSelf)
+        {
+            spongeTool.SetActive(false);
+            brushTool.SetActive(false);
+            shovelTool.SetActive(true);
+            wateringCanTool.SetActive(false);
+            clothTool.SetActive(false);
+        }
+
+        if (RockParent.childCount ==0 && shovelTool.gameObject.activeSelf)
+        {
+            spongeTool.SetActive(false);
+            brushTool.SetActive(false);
+            shovelTool.SetActive(false);
+            wateringCanTool.SetActive(true);
+            clothTool.SetActive(false);
+        }
+
+    }
+
+}
