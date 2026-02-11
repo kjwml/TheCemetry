915a4d0efd290cc93e4549bf9643e61212e496a4
KJ <myt9dwy5bv@privaterelay.appleid.com>
2026-02-11 11:09:37 +0100

    Dialog Grabsteinszene

---
+++ TheCemetry/Assets/Scripts/TriggetDialog.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ToolTyp
+{
+   Sponge,
+   Brush,
+   Shovel,
+   WateringCan,
+   Cloth
+}
+
+public class TriggerDialogue : MonoBehaviour
+{
+
+    public GameObject speechBubble;
+    public Text dialogueText;
+
+    public string[] dialogue;
+
+    public float wordSpeed = 0.05f;
+    public float bubbleVisibleTime = 2f;
+
+    private int index = 0;
+    private bool isTyping = false;
+
+    Vector2 originalScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speechBubble.SetActive(true);
+    }
+
+    public void UseTool(ToolTyp tool)
+    {
+    if ((int)tool != index) return;
+    StopAllCoroutines();
+    StartCoroutine (ShowDialogue());
+    }
+
+    IEnumerator ShowDialogue()
+    {
+        speechBubble.SetActive(true);
+        dialogueText.text = "";
+
+        foreach (char letter in dialogue[index])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+
+        yield return new WaitForSeconds(bubbleVisibleTime);
+
+        speechBubble.SetActive(false);
+        index++;
+    }
+}
